69891c7b6716786420ac071372d4dff8b9bfc711
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-12 12:41:16 +0700

    Remove unnecessary style

--- components/Banner.js
+++ components/Banner.js
@@ -10,19 +10,16 @@ export default ({ text, subText, paddingTop = '190px', imageClass }) => (
       </div>
     </div>
     <style jsx>{`
-    `}</style>
-    <style jsx global>{`
       .banner {
         height: 600px;
         color: #fff;
         text-shadow: 0px 2px 4px rgba(0,0,0,0.3);
       }
-      .banner h1 {
-        font-weight: 600;
-      }
       .banner h5 {
         letter-spacing: 0.1rem;
       }
+    `}</style>
+    <style jsx global>{`
       .iphone {
         width: 100%;
         height: 100px;
