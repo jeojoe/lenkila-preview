67d6e1dda07f41c989413b50ddad4264979aaba6
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-12 12:38:12 +0700

    Adjust banner style

--- components/Banner.js
+++ components/Banner.js
@@ -1,9 +1,9 @@
-export default ({ text, subText, paddingTop = '100px', imageClass }) => (
+export default ({ text, subText, paddingTop = '190px', imageClass }) => (
   <div className="banner" style={{ paddingTop }}>
     <div className="container">
       <div className="six columns">
-        <h3>{text}</h3>
-        <h6>{subText}</h6>
+        <h1>{text}</h1>
+        <h5>{subText}</h5>
       </div>
       <div className="six columns">
         <div className={imageClass} />
@@ -12,6 +12,17 @@ export default ({ text, subText, paddingTop = '100px', imageClass }) => (
     <style jsx>{`
     `}</style>
     <style jsx global>{`
+      .banner {
+        height: 600px;
+        color: #fff;
+        text-shadow: 0px 2px 4px rgba(0,0,0,0.3);
+      }
+      .banner h1 {
+        font-weight: 600;
+      }
+      .banner h5 {
+        letter-spacing: 0.1rem;
+      }
       .iphone {
         width: 100%;
         height: 100px;
