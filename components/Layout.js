06ec0d6afbefcb642dd73d8caeda8b1261d89de2
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-12 12:37:21 +0700

    Change font styles

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default ({ children, title = 'LENKILA : Thai Community For Sport Lovers' 
       <title>{ title }</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=Kanit:400,600i|Open+Sans:700i" rel="stylesheet" />
       <link rel="stylesheet" type="text/css" href="/static/normalize.css" />
       <link rel="stylesheet" type="text/css" href="/static/skeleton.css" />
     </Head>
